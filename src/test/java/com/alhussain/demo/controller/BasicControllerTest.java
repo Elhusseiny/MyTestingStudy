d64f33152326d0e984a1e16d2cdba2b761683907
Mohamed Elhussein <me.alhussain@gmail.com>
2022-08-13 04:00:19 +0200

    another API test (delete API )

--- src/test/java/com/alhussain/demo/controller/BasicControllerTest.java
+++ src/test/java/com/alhussain/demo/controller/BasicControllerTest.java
@@ -3,12 +3,12 @@ package com.alhussain.demo.controller;
 import com.alhussain.demo.model.Person;
 import com.alhussain.demo.repository.PersonRepository;
 import com.alhussain.demo.service.BasicService;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -20,14 +20,13 @@ import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@AutoConfigureMockMvc
 @SpringBootTest
 class BasicControllerTest {
 
-    @Mock
-    BasicService pesronService;
+    @MockBean
+    BasicService basicService;
 
-    @Mock
-    PersonRepository personRepository;
 
     @Autowired
     MockMvc mockMvc;
@@ -38,15 +37,11 @@ class BasicControllerTest {
         Person person = new Person();
         person.setId(5L);
         person.setName("Mohamed");
-        when(personRepository.findById(any())).thenReturn(Optional.of(person));
         mockMvc.perform(delete(
                         "/demo/person/{id}", 5L)
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk());
-        verify(personRepository).findById(any());
-        verify(personRepository).delete(person);
-
-
+        verify(basicService).deletePerson(5L);
 
     }
 
