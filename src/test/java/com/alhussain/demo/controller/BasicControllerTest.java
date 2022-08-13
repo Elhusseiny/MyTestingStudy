f980d31e14591602e90cc069eafc7b09aeb1c19d
Mohamed Elhussein <me.alhussain@gmail.com>
2022-08-13 09:10:08 +0200

    Using Active profiles for testing

--- src/test/java/com/alhussain/demo/controller/BasicControllerTest.java
+++ src/test/java/com/alhussain/demo/controller/BasicControllerTest.java
@@ -2,46 +2,29 @@ package com.alhussain.demo.controller;
 
 import com.alhussain.demo.model.Person;
 import com.alhussain.demo.repository.PersonRepository;
-import com.alhussain.demo.service.BasicService;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.mockito.Spy;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.context.ActiveProfiles;
 
-import java.util.Optional;
-
-import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@AutoConfigureMockMvc
+
 @SpringBootTest
+@ActiveProfiles("test")
 class BasicControllerTest {
 
-    @MockBean
-    BasicService basicService;
-
-
-    @Autowired
-    MockMvc mockMvc;
-
+    @Spy
+    PersonRepository personRepository;
 
     @Test
     public void shouldDeletePerson() throws Exception {
-        Person person = new Person();
-        person.setId(5L);
-        person.setName("Mohamed");
-        mockMvc.perform(delete(
-                        "/demo/person/{id}", 5L)
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk());
-        verify(basicService).deletePerson(5L);
+
+
+        personRepository.deleteById(1L);
+        //verify(personRepository, times(1)).deleteById(1L);
+        //verify(basicService).showMessage(); will fail the test
 
     }
 
